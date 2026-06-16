5bae0c236fbcdfde222a370cc75162090fbe2f21
Emanuel Kiplangat Kirui <emanuelkirui1@gmail.com>
2026-06-16 09:23:59 +0000

    update

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,10 +1,5 @@
 import { LoginForm } from "@/components/auth/login-form";
-import { LoginLayout } from "@/components/public/login-layout";
 
 export default function LoginPage() {
-  return (
-    <LoginLayout>
-      <LoginForm />
-    </LoginLayout>
-  );
+  return <LoginForm />;
 }
